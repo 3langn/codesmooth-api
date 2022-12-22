d715458cba264b5ea4d3276c874d494d52ea8390
Lang Minh Nguyen <3langn@gmail.com>
2022-12-23 01:33:39 +0700

    [update] remove youtube

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,12 +1,13 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
-import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
-import * as ytdl from 'ytdl-core';
-import { Readable } from 'typeorm/platform/PlatformTools';
+import { BadRequestException, Injectable, Logger } from "@nestjs/common";
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from "cloudinary";
+import { Readable } from "typeorm/platform/PlatformTools";
 
 @Injectable()
 export class UploadService {
   private readonly logger = new Logger(UploadService.name);
-  async uploadFilesToCloudinary(files: Array<Express.Multer.File>): Promise<(UploadApiResponse | UploadApiErrorResponse)[]> {
+  async uploadFilesToCloudinary(
+    files: Array<Express.Multer.File>
+  ): Promise<(UploadApiResponse | UploadApiErrorResponse)[]> {
     return await this.uploadImage(files).catch((e) => {
       this.logger.error(e);
       throw new BadRequestException(e.message);
@@ -17,7 +18,9 @@ export class UploadService {
     return Promise.all(files.map((file) => this.uploadStream(file)));
   }
 
-  async uploadStream(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  async uploadStream(
+    file: Express.Multer.File
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     try {
       return new Promise((resolve, reject) => {
         const upload = v2.uploader.upload_stream((error, result) => {
@@ -32,33 +35,42 @@ export class UploadService {
     }
   }
 
-  async uploadVideos(files: Array<Express.Multer.File>): Promise<(UploadApiResponse | UploadApiErrorResponse)[]> {
+  async uploadVideos(
+    files: Array<Express.Multer.File>
+  ): Promise<(UploadApiResponse | UploadApiErrorResponse)[]> {
     return Promise.all(
       files.map(
         async (file) =>
-          await new Promise<UploadApiResponse | UploadApiErrorResponse>((resolve, reject) => {
-            v2.uploader
-              .upload_stream({ resource_type: 'video', image_metadata: true }, (error, result) => {
-                if (error) return reject(error);
-                resolve(result);
-              })
-              .end(file.buffer);
-          }),
-      ),
+          await new Promise<UploadApiResponse | UploadApiErrorResponse>(
+            (resolve, reject) => {
+              v2.uploader
+                .upload_stream(
+                  { resource_type: "video", image_metadata: true },
+                  (error, result) => {
+                    if (error) return reject(error);
+                    resolve(result);
+                  }
+                )
+                .end(file.buffer);
+            }
+          )
+      )
     );
   }
 
-  async uploadFile(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  async uploadFile(
+    file: Express.Multer.File
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return;
   }
 
-  async youtubeUrlToMp3(url: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream({ resource_type: 'video', image_metadata: true }, (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-      });
-      ytdl(url, { filter: 'audioonly' }).pipe(upload);
-    });
-  }
+  // async youtubeUrlToMp3(url: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  //   return new Promise((resolve, reject) => {
+  //     const upload = v2.uploader.upload_stream({ resource_type: 'video', image_metadata: true }, (error, result) => {
+  //       if (error) return reject(error);
+  //       resolve(result);
+  //     });
+  //     ytdl(url, { filter: 'audioonly' }).pipe(upload);
+  //   });
+  // }
 }
