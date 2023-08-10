a15171d82f81a8d987c2081403c48214c53adcbd
Lang Minh Nguyên <3langn@gmail.com>
2023-08-10 11:29:42 +0700

    feat: add validate contact

--- src/modules/other/dto/contact.dto.ts
+++ src/modules/other/dto/contact.dto.ts
@@ -1,18 +1,25 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class ContactRequest {
   @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   name: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   @IsString()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   message: string;
 
   @IsString()
+  @IsNotEmpty()
   subject: string;
 
   @IsString()
+  @IsNotEmpty()
   phone: string;
 }
