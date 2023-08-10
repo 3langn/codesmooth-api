719d520e38e0b16b321371b52ec9048601cc40ae
Lang Minh Nguyên <3langn@gmail.com>
2023-08-10 11:43:24 +0700

    fix: missing phone in template

--- src/modules/other/dto/contact.dto.ts
+++ src/modules/other/dto/contact.dto.ts
@@ -1,25 +1,52 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from "class-validator";
 
 export class ContactRequest {
-  @IsString()
-  @IsEmail()
-  @IsNotEmpty()
+  @IsString({
+    message: "Tên phải là chuỗi",
+  })
+  @IsNotEmpty({
+    message: "Tên không được để trống",
+  })
   name: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-  @IsString()
+  @IsEmail(
+    {},
+    {
+      message: "Email không hợp lệ",
+    },
+  )
+  @IsNotEmpty({
+    message: "Email không được để trống",
+  })
+  @IsString({
+    message: "Email phải là chuỗi",
+  })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: "Nội dung phải là chuỗi",
+  })
+  @IsNotEmpty({
+    message: "Nội dung không được để trống",
+  })
   message: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: "Chủ đề phải là chuỗi",
+  })
+  @IsNotEmpty({
+    message: "Chủ đề không được để trống",
+  })
   subject: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: "Số điện thoại phải là chuỗi",
+  })
+  @IsNotEmpty({
+    message: "Số điện thoại không được để trống",
+  })
+  @IsPhoneNumber("VN", {
+    message: "Số điện thoại không hợp lệ",
+  })
   phone: string;
 }
